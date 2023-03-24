1ab2428c5087c3355f549e014ceab44c3ab8490a
BiscuitBobby <87699062+BiscuitBobby@users.noreply.github.com>
2023-03-24 18:21:06 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(app());
+}
+
+class app extends StatelessWidget {
+  const app({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+      backgroundColor: Colors.amber,
+      title: const Text('Flutter'),
+    )));
+  }
+}
