233e796ae3ead130652643ca828ca40a7767ff66
BiscuitBobby <87699062+BiscuitBobby@users.noreply.github.com>
2023-03-27 01:31:54 +0530

    added text field on left

--- lib/main.dart
+++ lib/main.dart
@@ -5,41 +5,48 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  final String imageUrl =
-      'https://imgs.search.brave.com/ha3j9EGBYvYBvSX2DCGWJ6IybccPkfoOj5R7z0FRwM8/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Uy/LmV4cGxpY2l0LmJp/bmcubmV0L3RoP2lk/PU9JUC4zQW5oTHRt/MkQyWDAxU3MxazBw/TDRBSGFIYSZwaWQ9/QXBp';
-  final double complete = 0.5; // 50% complete;
+  final String imageUrl = 'https://imgs.search.brave.com/ha3j9EGBYvYBvSX2DCGWJ6IybccPkfoOj5R7z0FRwM8/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Uy/LmV4cGxpY2l0LmJp/bmcubmV0L3RoP2lk/PU9JUC4zQW5oTHRt/MkQyWDAxU3MxazBw/TDRBSGFIYSZwaWQ9/QXBp';
+  final double complete = 0.4; // 50% complete;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Builder(
-        builder: (BuildContext context) {
-          return Directionality(
-            textDirection: TextDirection.ltr,
-            child: Container(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              child: Stack(
+      home: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
                 children: [
                   CircleAvatar(
                     radius: 100,
                     backgroundImage: NetworkImage(imageUrl),
                   ),
-                  Positioned(
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    child: LinearProgressIndicator(
-                      value: complete,
-                      backgroundColor: Colors.white,
-                      color: Colors.blue,
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: TextField(
+                        enabled: false,
+                        controller: TextEditingController(text: 'Last update: Made a flutter container'),
+                      )
                     ),
                   ),
                 ],
               ),
             ),
-          );
-        },
+            TextField(
+              enabled: false,
+              controller: TextEditingController(text: 'Progress bar'),
+            ),
+            SizedBox(
+              height: 20,
+              child: LinearProgressIndicator(
+                value: complete,
+                color: Colors.blue,
+                minHeight: 10,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
