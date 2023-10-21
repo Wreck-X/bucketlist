09a8423932601cd4675303b1107faba7cc08c742
Wreck-X <hemanthkrishnakumar@gmail.com>
2023-10-22 01:40:12 +0530

    status page structering

---
+++ lib/view/status_view.dart
@@ -0,0 +1,38 @@
+import 'package:bucketlist/utils/widgets/bottombar.dart';
+import 'package:flutter/material.dart';
+import '../resources/colors.dart';
+class StatusScreen extends StatefulWidget {
+  const StatusScreen({super.key});
+
+  @override
+  State<StatusScreen> createState() => _StatusScreenState();
+}
+
+class _StatusScreenState extends State<StatusScreen> {
+    @override
+        Widget build(BuildContext context) {
+            return Scaffold(
+                    backgroundColor: Colors.black,
+                    body: SafeArea(
+                      child: Column(
+                          children: [
+                          Container(
+                              height: 90,color: ColorsClass.purp,
+                          ),
+                          Expanded(
+                            child: Container(
+                                color: ColorsClass.black,
+                                child: ListView.builder(
+                                    itemCount: 10, // Temp count change on api call
+                                    itemBuilder: (context,index){
+                                    return Container(height: 100, child: Placeholder());
+                                    } 
+                                    )
+                                ),
+                          )],
+                          ),
+                    ),
+                    bottomNavigationBar:  SafeArea(child: BottomBar())
+                    );
+        }
+}
