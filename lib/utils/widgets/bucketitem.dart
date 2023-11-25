4fc19925beb5c804b843f2be7d5af50f4ff1a399
Wreck-X <hemanthkrishnakumar@gmail.com>
2023-11-25 23:10:55 +0530

    formatting

---
+++ lib/utils/widgets/bucketitem.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../../utils/Routes/route_names.dart';
+import '../../resources/colors.dart';
+
+class BucketItem extends StatelessWidget {
+  BucketItem({Key? key, required this.data, required this.index})
+      : super(key: key);
+  var data;
+  var index;
+  @override
+  Widget build(BuildContext context) {
+    print(data);
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10.0),
+      child: InkWell(
+        onTap: () {
+          Navigator.of(context).pushNamed(RouteNames.project);
+        },
+        child: Card(
+          color: GlobalTheme.backWidget,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              '' ?? 'Unnamed Project', // Display the project name
+              style: const TextStyle(color: GlobalTheme.foreground),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
