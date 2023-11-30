6923c1ea734869d624801e08a77fc97b9f5433e6
Aaryanajith <aaryanajith45@gmail.com>
2023-11-30 19:07:37 +0530

    add: shimmer effect while loading

--- lib/view/status_view.dart
+++ lib/view/status_view.dart
@@ -3,7 +3,7 @@ import 'package:bucketlist/utils/Routes/route_names.dart';
 import 'package:bucketlist/utils/widgets/statuscard.dart';
 import 'package:bucketlist/view_model/login_view_model.dart';
 import 'package:flutter/material.dart';
-import 'package:skeletonizer/skeletonizer.dart';
+import 'package:shimmer/shimmer.dart';
 import '../resources/colors.dart';
 
 class StatusScreen extends StatefulWidget {
@@ -14,6 +14,20 @@ class StatusScreen extends StatefulWidget {
 }
 
 class _StatusScreenState extends State<StatusScreen> {
+  bool _isloaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+
+    Future.delayed(const Duration(seconds: 3), () {
+      setState(() {
+        _isloaded = true;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -89,25 +103,21 @@ class _StatusScreenState extends State<StatusScreen> {
                 child: FutureBuilder(
                   future: getupdates(),
                   builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      // While waiting for data, return a loading indicator or placeholder
-                      return Center(child: CircularProgressIndicator());
-                    } else if (snapshot.hasError) {
+                    if (snapshot.hasError) {
                       // If there's an error, display an error message
                       return Text('Error: ${snapshot.error}');
                     } else {
-                      return Skeletonizer(
-                        enabled: ,
-                        child: ListView.builder(
-                          itemCount: snapshot.data?.length,
-                          itemBuilder: (context, index) {
-                            return (StatusCard(
-                              content: snapshot.data,
-                              index: index,
-                            ));
-                          },
-                        ),
-                      );
+                      return _isloaded
+                          ? ListView.builder(
+                              itemCount: snapshot.data?.length,
+                              itemBuilder: (context, index) {
+                                return StatusCard(
+                                  content: snapshot.data,
+                                  index: index,
+                                );
+                              },
+                            )
+                          : loadingShimer();
                     }
                   },
                 ),
@@ -124,9 +134,9 @@ class _StatusScreenState extends State<StatusScreen> {
             onPressed: () {
               ;
             },
-            child: Icon(Icons.add),
+            child: const Icon(Icons.add),
           ),
-          SizedBox(height: 10),
+          const SizedBox(height: 10),
           FloatingActionButton(
             backgroundColor: ColorsClass.purp,
             onPressed: () {
@@ -141,5 +151,122 @@ class _StatusScreenState extends State<StatusScreen> {
         ],
       ),
     );
+  }
+
+  loadingShimer() {
+    return ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return Column(children: [
+            const SizedBox(height: 10),
+            Container(
+              decoration: BoxDecoration(
+                  color: const Color.fromRGBO(30, 30, 30, 1),
+                  borderRadius: BorderRadius.circular(10)),
+              padding: const EdgeInsets.all(20),
+              height: MediaQuery.of(context).size.height * 0.18,
+              width: MediaQuery.of(context).size.width * 0.96,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(5),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 30,
+                          width: 20,
+                          child: Shimmer.fromColors(
+                            baseColor: Colors.grey[300]!,
+                            highlightColor:
+                                const Color.fromARGB(255, 123, 123, 123),
+                            child: const Icon(
+                              Icons.person,
+                              color: Colors.purple,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Shimmer.fromColors(
+                          baseColor: Colors.grey[300]!,
+                          highlightColor:
+                              const Color.fromARGB(255, 123, 123, 123),
+                          child: Container(
+                            height: 10,
+                            width: 40,
+                            color: Colors.grey,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Row(children: [
+                    SizedBox(
+                        height: 30,
+                        width: 30,
+                        child: Shimmer.fromColors(
+                          baseColor: Colors.grey[300]!,
+                          highlightColor: Color.fromARGB(255, 123, 123, 123),
+                          child: const Icon(
+                            Icons.pending_outlined,
+                            color: Colors.yellow,
+                            size: 30,
+                          ),
+                        )),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      children: [
+                        Shimmer.fromColors(
+                          highlightColor:
+                              const Color.fromARGB(255, 123, 123, 123),
+                          baseColor: Colors.grey[300]!,
+                          child: Container(
+                            height: 10,
+                            width: 100,
+                            color: Colors.grey,
+                          ),
+                        )
+                      ],
+                    )
+                  ]),
+                  Row(
+                    children: [
+                      SizedBox(
+                        height: 30,
+                        width: 30,
+                        child: Shimmer.fromColors(
+                          baseColor: Colors.grey[300]!,
+                          highlightColor:
+                              const Color.fromARGB(255, 123, 123, 123),
+                          child: const Icon(
+                            Icons.play_circle_outline,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Shimmer.fromColors(
+                        baseColor: Colors.grey[300]!,
+                        highlightColor:
+                            const Color.fromARGB(255, 123, 123, 123),
+                        child: Container(
+                          height: 10,
+                          width: 100,
+                          color: Colors.grey,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ]);
+        });
   }
 }
